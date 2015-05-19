b2ac99b4d4721c326f0ba4bfba95c576c2189158
Simon Moles <simon@stompyrobot.uk>
2015-05-19 23:21:35 +0100

    Update AssemblyInfo

--- Src/PrecisionEngineering/Properties/AssemblyInfo.cs
+++ Src/PrecisionEngineering/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RoadEngineer")]
+[assembly: AssemblyTitle("Precision Engineering")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RoadEngineer")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("Precision Engineering")]
+[assembly: AssemblyCopyright("Copyright © Simon Moles 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
