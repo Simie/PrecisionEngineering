b450da2da79219572dd18459ad24f7c42103462c
Simon Moles <simon@stompyrobot.uk>
2015-05-27 17:32:41 +0100

    Oops, missing change

--- Src/PrecisionEngineering/Data/Calculations/Guides.cs
+++ Src/PrecisionEngineering/Data/Calculations/Guides.cs
@@ -29,7 +29,7 @@ namespace PrecisionEngineering.Data.Calculations
 				//var segments = NetManager.instance.m_segments;
 				//NetManager.instance.GetClosestSegments(endPosition, SegmentCache, out _segmentCacheCount);
 
-				GetClosestSegments(netInfo, endPosition, SegmentCache, out _segmentCacheCount);
+				NetUtil.GetClosestSegments(netInfo, endPosition, SegmentCache, out _segmentCacheCount);
 				SnapController.DebugPrint = String.Format("Closest Segment Count: {0}", _segmentCacheCount);
 
 				var c = _segmentCacheCount;
@@ -251,83 +251,6 @@ namespace PrecisionEngineering.Data.Calculations
 			measurements.Add(new AngleMeasurement(otherAngleSize, anglePosition, otherAngleDirection,
 				MeasurementFlags.Secondary | MeasurementFlags.Guide));
 
-		}
-
-
-		/// <summary>
-		/// A modified version of the NetManager.GetClosestSegments method which filters by the NetInfo class
-		/// </summary>
-		/// <param name="netInfo"></param>
-		/// <param name="pos"></param>
-		/// <param name="segments"></param>
-		/// <param name="count"></param>
-		private static void GetClosestSegments(NetInfo netInfo, Vector3 pos, ushort[] segments, out int count)
-		{
-
-			var nm = NetManager.instance;
-
-			count = 0;
-			float searchRange = 32f;
-
-			for (int i = 0; i < 5; ++i) {
-				float xMin = pos.x - searchRange;
-				float zMin = pos.z - searchRange;
-				float xMax = pos.x + searchRange;
-				float zMax = pos.z + searchRange;
-
-				int xTileMin = Mathf.Max((int) ((xMin - 64.0)/64.0 + 135.0), 0);
-				int zTileMin = Mathf.Max((int) ((zMin - 64.0)/64.0 + 135.0), 0);
-				int xTileMax = Mathf.Min((int) ((xMax + 64.0)/64.0 + 135.0), 269);
-				int zTileMax = Mathf.Min((int) ((zMax + 64.0)/64.0 + 135.0), 269);
-
-				for (int zTile = zTileMin; zTile <= zTileMax; ++zTile) {
-					for (int xTile = xTileMin; xTile <= xTileMax; ++xTile) {
-						ushort segmentId = nm.m_segmentGrid[zTile*270 + xTile];
-						int num11 = 0;
-						while (segmentId != 0) {
-							ushort startNodeId = nm.m_segments.m_buffer[segmentId].m_startNode;
-							ushort endNodeId = nm.m_segments.m_buffer[segmentId].m_endNode;
-							Vector3 startPosition = nm.m_nodes.m_buffer[startNodeId].m_position;
-							Vector3 endPosition = nm.m_nodes.m_buffer[endNodeId].m_position;
-							if (NetUtil.AreSimilarClass(nm.m_segments.m_buffer[segmentId].Info, netInfo) && (
-								(Mathf.Max(Mathf.Max(xMin - 64f - startPosition.x, zMin - 64f - startPosition.z),
-									Mathf.Max((float) (startPosition.x - (double) xMax - 64.0),
-										(float) (startPosition.z - (double) zMax - 64.0))) < 0.0 ||
-								 Mathf.Max(Mathf.Max(xMin - 64f - endPosition.x, zMin - 64f - endPosition.z),
-									 Mathf.Max((float) (endPosition.x - (double) xMax - 64.0),
-										 (float) (endPosition.z - (double) zMax - 64.0))) < 0.0) &&
-								(Mathf.Min(startPosition.x, endPosition.x) <= (double) xMax &&
-								 Mathf.Min(startPosition.z, endPosition.z) <= (double) zMax) &&
-								(Mathf.Max(startPosition.x, endPosition.x) >= (double) xMin &&
-								 Mathf.Max(startPosition.z, endPosition.z) >= (double) zMin))) {
-								bool isNew = true;
-								for (int j = 0; j < count; ++j) {
-									if (segments[j] == segmentId) {
-										isNew = false;
-										break;
-									}
-								}
-								if (isNew) {
-
-									segments[count++] = segmentId;
-
-									if (count == segments.Length)
-										return;
-
-								}
-							}
-							segmentId = nm.m_segments.m_buffer[segmentId].m_nextGridSegment;
-
-							if (++num11 >= 32768) {
-								CODebugBase<LogChannel>.Error(LogChannel.Core, "Invalid list detected!\n" + Environment.StackTrace);
-								break;
-							}
-
-						}
-					}
-				}
-				searchRange *= 2f;
-			}
 		}
 
 	}
